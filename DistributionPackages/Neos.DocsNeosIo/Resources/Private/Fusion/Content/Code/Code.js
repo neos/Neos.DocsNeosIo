31560b3b48d2a2d0a37b11dae28fc8c466ca0b03
rolandschuetz <mail@rolandschuetz.at>
2018-12-06 18:23:58 +0100

    TASK: Improved code formatting

--- DistributionPackages/Neos.DocsNeosIo/Resources/Private/Fusion/Content/Code/Code.js
+++ DistributionPackages/Neos.DocsNeosIo/Resources/Private/Fusion/Content/Code/Code.js
@@ -1,17 +1,6 @@
-import '../../../../Public/Frontend/node_modules/prismjs/prism.js';
-import '../../../../Public/Frontend/node_modules/prismjs/components/prism-yaml.js';
-import '../../../../Public/Frontend/node_modules/prismjs/components/prism-markup-templating.js'; // required for php
-import '../../../../Public/Frontend/node_modules/prismjs/components/prism-php.js';
-import '../../../../Public/Frontend/node_modules/prismjs/components/prism-bash.js';
-import '../../../../Public/Frontend/node_modules/prismjs/components/prism-scss.js';
-import '../../../../Public/Frontend/node_modules/prismjs/components/prism-sass.js';
-import '../../../../Public/Frontend/node_modules/prismjs/components/prism-typescript.js';
-import '../../../../Public/Frontend/node_modules/prismjs/components/prism-sql.js';
-import '../../../../Public/Frontend/node_modules/prismjs/components/prism-markdown.js';
-
 (function() {
 	function highlightElements() {
-		let codeBlocks = Array.from(document.querySelectorAll('.code code'));
+		let codeBlocks = Array.from(document.querySelectorAll('.code pre'));
 		codeBlocks.forEach(element => Prism.highlightElement(element));
 	}
 
--- DistributionPackages/Neos.DocsNeosIo/Resources/Public/Frontend/js/app.js
+++ DistributionPackages/Neos.DocsNeosIo/Resources/Public/Frontend/js/app.js
@@ -1,6 +1,22 @@
 
 import './materialize-docs.js';
 
+// prism code highlighting
+import '../node_modules/prismjs/prism.js';
+import '../node_modules/prismjs/components/prism-yaml.js';
+import '../node_modules/prismjs/components/prism-markup-templating.js'; // required for php
+import '../node_modules/prismjs/components/prism-php.js';
+import '../node_modules/prismjs/components/prism-bash.js';
+import '../node_modules/prismjs/components/prism-scss.js';
+import '../node_modules/prismjs/components/prism-sass.js';
+import '../node_modules/prismjs/components/prism-typescript.js';
+import '../node_modules/prismjs/components/prism-sql.js';
+import '../node_modules/prismjs/components/prism-markdown.js';
+import '../node_modules/prismjs/plugins/autolinker/prism-autolinker.js';
+import '../node_modules/prismjs/plugins/line-numbers/prism-line-numbers.js';
+// import '../node_modules/prismjs/plugins/toolbar/prism-toolbar.js';
+// import '../node_modules/prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.js';
+
 // Components
 import '../../../Private/Fusion/Component/Header/Header.js';
 import '../../../Private/Fusion/Component/LocalNavigation/LocalNavigation.js';
