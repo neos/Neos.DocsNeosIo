c2bd49fcf386c23ff1de82a00dba6bc9a59d04f4
Roland Schütz <mail@rolandschuetz.at>
2019-03-23 13:35:24 +0100

    TASK: Replace uglifyjs with terser for ES6 support

--- DistributionPackages/Neos.DocsNeosIo/Resources/Public/Frontend/rollup.config.js
+++ DistributionPackages/Neos.DocsNeosIo/Resources/Public/Frontend/rollup.config.js
@@ -1,11 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
-import { uglify } from 'rollup-plugin-uglify';
-
-// `npm run build` -> `production` is true
-// `npm run dev` -> `production` is false
-const production = !process.env.ROLLUP_WATCH;
-
+import { terser } from 'rollup-plugin-terser';
 
 export default {
     input: 'js/app.js',
@@ -15,13 +10,13 @@ export default {
         sourcemap: true
     },
     watch: {
-        include: 'js/**'
+		include: '../../../../**/*.js'
     },
     plugins: [
         resolve(),
         babel({
             exclude: 'node_modules/**' // only transpile our source code
         }),
-        production && uglify() // minify, but only in production
+        terser()
     ]
 };
