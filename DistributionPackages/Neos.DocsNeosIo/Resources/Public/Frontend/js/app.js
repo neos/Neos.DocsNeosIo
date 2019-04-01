f1a7ae6bbf037267b1890cfd709a43e6c5a7c7f6
Roland Schütz <mail@rolandschuetz.at>
2019-04-02 01:05:34 +0200

    TASK: Remove Code line numbers and enable xml support

--- DistributionPackages/Neos.DocsNeosIo/Resources/Public/Frontend/js/app.js
+++ DistributionPackages/Neos.DocsNeosIo/Resources/Public/Frontend/js/app.js
@@ -14,7 +14,7 @@ import '../node_modules/prismjs/components/prism-typescript.js';
 import '../node_modules/prismjs/components/prism-sql.js';
 import '../node_modules/prismjs/components/prism-markdown.js';
 import '../node_modules/prismjs/plugins/autolinker/prism-autolinker.js';
-import '../node_modules/prismjs/plugins/line-numbers/prism-line-numbers.js';
+// import '../node_modules/prismjs/plugins/line-numbers/prism-line-numbers.js';
 // import '../node_modules/prismjs/plugins/toolbar/prism-toolbar.js';
 // import '../node_modules/prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.js';
 import "../node_modules/@fortawesome/fontawesome-free/js/fontawesome.js";
